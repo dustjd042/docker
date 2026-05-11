f725d2643647f5c2328b63946ff4aa1066411a1b
dustjd042 <74745159+dustjd042@users.noreply.github.com>
2026-05-11 14:30:47 +0900

    Update cgroup.sh

--- container/cgroup.sh
+++ container/cgroup.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-# cgroup 파일 생성 (커널이 기본적인 파일 항목 생성)
-mkdir /sys/fs/cgroup/cpu/red;
-mkdir /sys/fs/cgroup/memory/red;
-
-# cgroup 자원 설정 (CPU: 40%, RAM: 200MB, 메모리 스왑 제한)
-echo 40000 > /sys/fs/cgroup/cpu/red/cpu.cfs_quota_us;
-echo 209715200 > /sys/fs/cgroup/memory/red/memory.limit_in_bytes;
-echo 0 > /sys/fs/cgroup/memory/red/memory.swappiness;
+CGROUP_NAME=$1
 
 # cgroup 파일 생성 (커널이 기본적인 파일 항목 생성)
-mkdir /sys/fs/cgroup/cpu/blue;
-mkdir /sys/fs/cgroup/memory/blue;
+mkdir -p /sys/fs/cgroup/cpu/$CGROUP_NAME;
+mkdir -p /sys/fs/cgroup/memory/$CGROUP_NAME;
 
 # cgroup 자원 설정 (CPU: 40%, RAM: 200MB, 메모리 스왑 제한)
-echo 40000 > /sys/fs/cgroup/cpu/blue/cpu.cfs_quota_us;
-echo 209715200 > /sys/fs/cgroup/memory/blue/memory.limit_in_bytes;
-echo 0 > /sys/fs/cgroup/memory/blue/memory.swappiness;
+echo 40000 > /sys/fs/cgroup/cpu/$CGROUP_NAME/cpu.cfs_quota_us;
+echo 209715200 > /sys/fs/cgroup/memory/$CGROUP_NAME/memory.limit_in_bytes;
+echo 0 > /sys/fs/cgroup/memory/$CGROUP_NAME/memory.swappiness;
